024b623fde229284623fdd6ec1740fc170140643
quic-rnamburi <quic-rnamburi@users.noreply.github.com>
2026-08-30 20:07:25 +0000

    docs: update API reference for audioreach-engine @ e9191b39afb06744805afaa9dcdc817b180e5109 2db808c783f8f9fefc111a56c5942563082c0461

---
+++ audioreach-engine/navtreedata.js
@@ -0,0 +1,65 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "audioreach-engine", "index.html", [
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"amdb__api_8h.html",
+"ar__spf__cmd__api_8h.html#a3946fea47395e187a98e805689cf6708",
+"globals_t.html",
+"media__fmt__api__basic_8h.html#abd710d003829b1af0ac49a6d97f1b803a3a88a9c1ca6f19da29bff66d77b85f10",
+"media__fmt__api__ext_8h.html#ad2e6633f1bbd022769bcf5355753ca72",
+"structapm__cont__prop__id__parent__container__t.html",
+"structdetection__timestamp__info__t.html#af015f306c9a36b953e20b1fadd03d3db",
+"structpayload__media__fmt__asr__t.html#a9ede502b34eb8f39ea5a11e067c0db9e",
+"vcpm__api_8h.html#a03eaa82c2242df91b7243a4c1605a8f5"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
